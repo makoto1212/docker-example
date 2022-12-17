04db70557bc2d748f3f3f9e1d29165a27d2b56a6
makoto1212 <6114697+makoto1212@users.noreply.github.com>
2022-12-18 01:30:13 +0900

    テーブル定義のCOLLATEを修正

--- mysql/init/1_create_table_pokemons.sql
+++ mysql/init/1_create_table_pokemons.sql
@@ -2,4 +2,4 @@ CREATE TABLE `pokemons` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
